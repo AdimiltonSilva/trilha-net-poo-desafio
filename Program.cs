34b8115a24707de759f7f222e29bd7b635450a97
Adimilton Antonio <adimilton@gmail.com>
2024-01-09 00:32:08 -0300

    Desafio - Trilha .NET POO - Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "(81)98837-4804", modelo: "Nokia G11 Plus NK095", imei: "202401101", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "(81)98837-4804", modelo: "iPhone 15 Pro Max", imei: "202401102", memoria: 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
